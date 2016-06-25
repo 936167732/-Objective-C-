03b097623037e23ba1e2edb4739125439b3c6977
谢海邦 <X@xiehaibangdemacbook-pro.local>
2016-06-26 07:51:02 +0800

    Initial Commit

---
+++ 第一个Objective-C程序/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  第一个Objective-C程序
+//
+//  Created by 谢海邦 on 16/6/26.
+//  Copyright © 2016年 谢海邦. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
